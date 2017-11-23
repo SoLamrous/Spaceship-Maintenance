8350a044a08e909a505038ef0afa739588803fde
slamrous <sofiane.lamrous@etu.utc.fr>
2017-11-23 19:53:33 +0100

    Lvl_saisi

---
+++ SpaceShip M/Assets/CheckReussi.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckReussi : MonoBehaviour {
+	public GameObject c;
+	public Text t;
+	Rigidbody r;
+	// Use this for initialization
+	void Start () {
+		r = GetComponent<Rigidbody>();
+		//r.AddForce(-transform.right * 200f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("Zone")) { 
+			t.text = "Done !";
+
+		}
+	}
+}
